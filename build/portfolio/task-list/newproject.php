c37f6edb3dbc9c6b072857bf1e40e823f1bf0ded
jesse gauthier <jesse@gauthierstudios.ca>
2023-03-23 10:55:26 -0400

    .

--- build/portfolio/task-list/newproject.php
+++ build/portfolio/task-list/newproject.php
@@ -1,6 +1,6 @@
 <?php
 // database connection code
-$con = mysqli_connect('localhost:3306', 'project_manager', 'Bailey1967!!', 'project_tracker');
+$con = mysqli_connect('3.229.108.115:3306', 'project_manager', 'Bailey1967!!', 'project_tracker');
 
 // Check connection
 if (mysqli_connect_errno()) {
